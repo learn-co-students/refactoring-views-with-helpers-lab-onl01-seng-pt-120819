412d5a6ad3b8388dc074bffbba890e20c13c8fee
Amanda <main@Amandas-MacBook-Air.local>
2020-05-12 11:17:04 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    current_artist = Artist.find_or_create_by(name: name)
+    self.artist = current_artist
+
   end
 end
